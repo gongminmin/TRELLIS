4f8c94cccf67bd382cdff81f09ead3ea66a66626
Minmin Gong <gongminmin@msn.com>
2025-02-19 22:36:53 -0800

    Remve opt bake texture

--- trellis/utils/postprocessing_utils.py
+++ trellis/utils/postprocessing_utils.py
@@ -2,7 +2,6 @@ from typing import *
 import numpy as np
 import torch
 import utils3d
-import nvdiffrast.torch as dr
 from tqdm import tqdm
 import trimesh
 import trimesh.visual
@@ -10,7 +9,6 @@ import xatlas
 import pyvista as pv
 import cv2
 from PIL import Image
-from .random_utils import sphere_hammersley_sequence
 from .render_utils import render_multiview
 from ..representations import MeshExtractResult
 
@@ -80,7 +78,6 @@ def bake_texture(
     texture_size: int = 2048,
     near: float = 0.1,
     far: float = 10.0,
-    mode: Literal['fast', 'opt'] = 'opt',
     lambda_tv: float = 1e-2,
     verbose: bool = False,
 ):
@@ -98,7 +95,6 @@ def bake_texture(
         texture_size (int): Size of the texture.
         near (float): Near plane of the camera.
         far (float): Far plane of the camera.
-        mode (Literal['fast', 'opt']): Mode of texture baking.
         lambda_tv (float): Weight of total variation loss in optimization.
         verbose (bool): Whether to print progress.
     """
@@ -110,90 +106,37 @@ def bake_texture(
     views = [utils3d.torch.extrinsics_to_view(torch.tensor(extr).cuda()) for extr in extrinsics]
     projections = [utils3d.torch.intrinsics_to_perspective(torch.tensor(intr).cuda(), near, far) for intr in intrinsics]
 
-    if mode == 'fast':
-        texture = torch.zeros((texture_size * texture_size, 3), dtype=torch.float32).cuda()
-        texture_weights = torch.zeros((texture_size * texture_size), dtype=torch.float32).cuda()
-        rastctx = utils3d.torch.RastContext(backend='cuda')
-        for observation, view, projection in tqdm(zip(observations, views, projections), total=len(observations), disable=not verbose, desc='Texture baking (fast)'):
-            with torch.no_grad():
-                rast = utils3d.torch.rasterize_triangle_faces(
-                    rastctx, vertices[None], faces, observation.shape[1], observation.shape[0], uv=uvs[None], view=view, projection=projection
-                )
-                uv_map = rast['uv'][0].detach().flip(0)
-                mask = rast['mask'][0].detach().bool() & masks[0]
-            
-            # nearest neighbor interpolation
-            uv_map = (uv_map * texture_size).floor().long()
-            obs = observation[mask]
-            uv_map = uv_map[mask]
-            idx = uv_map[:, 0] + (texture_size - uv_map[:, 1] - 1) * texture_size
-            texture = texture.scatter_add(0, idx.view(-1, 1).expand(-1, 3), obs)
-            texture_weights = texture_weights.scatter_add(0, idx, torch.ones((obs.shape[0]), dtype=torch.float32, device=texture.device))
+    texture = torch.zeros((texture_size * texture_size, 3), dtype=torch.float32).cuda()
+    texture_weights = torch.zeros((texture_size * texture_size), dtype=torch.float32).cuda()
+    rastctx = utils3d.torch.RastContext(backend='cuda')
+    for observation, view, projection in tqdm(zip(observations, views, projections), total=len(observations), disable=not verbose, desc='Texture baking (fast)'):
+        with torch.no_grad():
+            rast = utils3d.torch.rasterize_triangle_faces(
+                rastctx, vertices[None], faces, observation.shape[1], observation.shape[0], uv=uvs[None], view=view, projection=projection
+            )
+            uv_map = rast['uv'][0].detach().flip(0)
+            mask = rast['mask'][0].detach().bool() & masks[0]
 
-        mask = texture_weights > 0
-        texture[mask] /= texture_weights[mask][:, None]
-        texture = np.clip(texture.reshape(texture_size, texture_size, 3).cpu().numpy() * 255, 0, 255).astype(np.uint8)
+        # nearest neighbor interpolation
+        uv_map = (uv_map * texture_size).floor().long()
+        obs = observation[mask]
+        uv_map = uv_map[mask]
+        idx = uv_map[:, 0] + (texture_size - uv_map[:, 1] - 1) * texture_size
+        texture = texture.scatter_add(0, idx.view(-1, 1).expand(-1, 3), obs)
+        texture_weights = texture_weights.scatter_add(0, idx, torch.ones((obs.shape[0]), dtype=torch.float32, device=texture.device))
 
-        # inpaint
-        mask = (texture_weights == 0).cpu().numpy().astype(np.uint8).reshape(texture_size, texture_size)
-        texture = cv2.inpaint(texture, mask, 3, cv2.INPAINT_TELEA)
+    mask = texture_weights > 0
+    texture[mask] /= texture_weights[mask][:, None]
+    texture = np.clip(texture.reshape(texture_size, texture_size, 3).cpu().numpy() * 255, 0, 255).astype(np.uint8)
 
-    elif mode == 'opt':
-        rastctx = utils3d.torch.RastContext(backend='cuda')
-        observations = [observations.flip(0) for observations in observations]
-        masks = [m.flip(0) for m in masks]
-        _uv = []
-        _uv_dr = []
-        for observation, view, projection in tqdm(zip(observations, views, projections), total=len(views), disable=not verbose, desc='Texture baking (opt): UV'):
-            with torch.no_grad():
-                rast = utils3d.torch.rasterize_triangle_faces(
-                    rastctx, vertices[None], faces, observation.shape[1], observation.shape[0], uv=uvs[None], view=view, projection=projection
-                )
-                _uv.append(rast['uv'].detach())
-                _uv_dr.append(rast['uv_dr'].detach())
-
-        texture = torch.nn.Parameter(torch.zeros((1, texture_size, texture_size, 3), dtype=torch.float32).cuda())
-        optimizer = torch.optim.Adam([texture], betas=(0.5, 0.9), lr=1e-2)
-
-        def exp_anealing(optimizer, step, total_steps, start_lr, end_lr):
-            return start_lr * (end_lr / start_lr) ** (step / total_steps)
-
-        def cosine_anealing(optimizer, step, total_steps, start_lr, end_lr):
-            return end_lr + 0.5 * (start_lr - end_lr) * (1 + np.cos(np.pi * step / total_steps))
-        
-        def tv_loss(texture):
-            return torch.nn.functional.l1_loss(texture[:, :-1, :, :], texture[:, 1:, :, :]) + \
-                   torch.nn.functional.l1_loss(texture[:, :, :-1, :], texture[:, :, 1:, :])
-    
-        total_steps = 2500
-        with tqdm(total=total_steps, disable=not verbose, desc='Texture baking (opt): optimizing') as pbar:
-            for step in range(total_steps):
-                optimizer.zero_grad()
-                selected = np.random.randint(0, len(views))
-                uv, uv_dr, observation, mask = _uv[selected], _uv_dr[selected], observations[selected], masks[selected]
-                render = dr.texture(texture, uv, uv_dr)[0]
-                loss = torch.nn.functional.l1_loss(render[mask], observation[mask])
-                if lambda_tv > 0:
-                    loss += lambda_tv * tv_loss(texture)
-                loss.backward()
-                optimizer.step()
-                # annealing
-                optimizer.param_groups[0]['lr'] = cosine_anealing(optimizer, step, total_steps, 1e-2, 1e-5)
-                pbar.set_postfix({'loss': loss.item()})
-                pbar.update()
-        texture = np.clip(texture[0].flip(0).detach().cpu().numpy() * 255, 0, 255).astype(np.uint8)
-        mask = 1 - utils3d.torch.rasterize_triangle_faces(
-            rastctx, (uvs * 2 - 1)[None], faces, texture_size, texture_size
-        )['mask'][0].detach().cpu().numpy().astype(np.uint8)
-        texture = cv2.inpaint(texture, mask, 3, cv2.INPAINT_TELEA)
-    else:
-        raise ValueError(f'Unknown mode: {mode}')
+    # inpaint
+    mask = (texture_weights == 0).cpu().numpy().astype(np.uint8).reshape(texture_size, texture_size)
+    texture = cv2.inpaint(texture, mask, 3, cv2.INPAINT_TELEA)
 
     return texture
 
 
 def to_glb(
-    app_rep: MeshExtractResult,
     mesh: MeshExtractResult,
     simplify: float = 0.95,
     texture_size: int = 1024,
@@ -203,7 +146,6 @@ def to_glb(
     Convert a generated asset to a glb file.
 
     Args:
-        app_rep (MeshExtractResult): Appearance representation.
         mesh (MeshExtractResult): Extracted mesh.
         simplify (float): Ratio of faces to remove in simplification.
         texture_size (int): Size of the texture.
@@ -224,14 +166,14 @@ def to_glb(
     vertices, faces, uvs = parametrize_mesh(vertices, faces)
 
     # bake texture
-    observations, extrinsics, intrinsics = render_multiview(app_rep, resolution=1024, nviews=100)
+    observations, extrinsics, intrinsics = render_multiview(mesh, resolution=1024, nviews=100)
     masks = [np.any(observation > 0, axis=-1) for observation in observations]
     extrinsics = [extrinsics[i].cpu().numpy() for i in range(len(extrinsics))]
     intrinsics = [intrinsics[i].cpu().numpy() for i in range(len(intrinsics))]
     texture = bake_texture(
         vertices, faces, uvs,
         observations, masks, extrinsics, intrinsics,
-        texture_size=texture_size, mode='opt',
+        texture_size=texture_size,
         lambda_tv=0.01,
         verbose=verbose
     )
